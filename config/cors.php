8c46c12b32fceecbb8793a8a7a7a0d1519495abd
espaya <beehivelive8@gmail.com>
2025-08-06 01:05:00 +0000

    Edited cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,16 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
-        'http://localhost:3000', 
-        'https://lmsapi.1staccesshomecareinc.com', 
-        'https://lms.1staccesshomecareinc.com'
+        'http://localhost:3000',
+        'https://lmsapi.1staccesshomecareinc.com',
+        'https://lms.1staccesshomecareinc.com',
     ],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
