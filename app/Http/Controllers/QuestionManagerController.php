4a481713bd97ed88b82ca209ee39134fe5ed6f79
espaya <beehivelive8@gmail.com>
2025-08-26 14:50:11 +0000

    fixed file path

--- app/Http/Controllers/QuestionManagerController.php
+++ app/Http/Controllers/QuestionManagerController.php
@@ -181,7 +181,7 @@ class QuestionManagerController extends Controller
 
             // send email to all users
             $users = User::where('role', 'USER')->get();
-            $fileUrl = asset('storage/questions/' . $fileName);
+            $fileUrl = asset('public/storage/questions/' . $fileName);
             foreach ($users as $user) {
                 Mail::to($user->email)->send(new QuestionUploadMail($user, $fileUrl));
             }
