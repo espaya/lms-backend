bc898890133a8f5d73def334ff745860dd658537
espaya <beehivelive8@gmail.com>
2025-11-28 00:51:09 +0000

    Fixed store method

--- app/Http/Controllers/SwornDisclosureController.php
+++ app/Http/Controllers/SwornDisclosureController.php
@@ -56,50 +56,33 @@ class SwornDisclosureController extends Controller
             'wit_signature' => 'required',
             'signature' => 'required',
         ], [
-            'mailing_address.required' => 'This field is required',
-            'convicted_outside_commonwealth.required' => 'This field is required',
-            'convicted_pending.required' => 'This field is required',
-            'child_abuse.required' => 'This field is required',
-            'wit_signature.required' => 'This field is required',
-            'signature.required' => 'This field is required',
+            'mailing_address.required' => 'This field is required.',
+            'convicted_outside_commonwealth.required' => 'This field is required.',
+            'convicted_pending.required' => 'This field is required.',
+            'child_abuse.required' => 'This field is required.',
+            'wit_signature.required' => 'Witness signature is required.',
+            'signature.required' => 'Applicant signature is required.',
         ]);
+
 
         DB::beginTransaction();
 
         try {
-            $swornDisclosure = new SwornDisclosure();
 
-            // get signature input
-            $signatureData = $request->input('signature');
-            $witSignatureData = $request->input('wit_signature');
+            // decode signatures
+            $signatureBinary = base64_decode(explode(',', $request->signature)[1]);
+            $witSignatureBinary = base64_decode(explode(',', $request->wit_signature)[1]);
 
-            $signatureParts = explode(',', $signatureData);
-            $signatureEncoded = $signatureParts[1]; // Extract the base64-encoded part
-            $witSignatureParts = explode(',', $witSignatureData);
-            $witSignatureEncoded = $witSignatureParts[1];
-
-            $signatureBinary = base64_decode($signatureEncoded);
-            $witSignatureBinary = base64_decode($witSignatureEncoded);
-
-            // Generate a unique filename for the signature file
             $signatureName = time() . '.png';
-            $witSignatureName = time() . '_.png';
+            $witSignatureName = time() . '_wit.png';
 
-            // Save the signature file to disk using the Storage facade
-            $singaturePath = storage_path('app/public/signature');
+            $path = storage_path('app/public/signature');
+            if (!File::exists($path)) {
+                File::makeDirectory($path, 0755, true);
+            }
 
-
-            $swornDisclosure->applicant_id = $userID;
-            $swornDisclosure->mailing_address = $request->mailing_address;
-            $swornDisclosure->convicted_outside_commonwealth = $request->convicted_outside_commonwealth;
-            $swornDisclosure->outside_commonwealth_specify = $request->outside_commonwealth_specify;
-            $swornDisclosure->convicted_pending = $request->convicted_pending;
-            $swornDisclosure->convicted_pending_specify = $request->convicted_pending_specify;
-            $swornDisclosure->child_abuse = $request->child_abuse;
-            $swornDisclosure->signature = $signatureName;
-            $swornDisclosure->wit_signature = $witSignatureName;
-
-            SwornDisclosure::firstOrCreate(
+            // Save/Update database
+            $swornDisclosure = SwornDisclosure::updateOrCreate(
                 ['applicant_id' => $userID],
                 [
                     'mailing_address' => $request->mailing_address,
@@ -110,24 +93,22 @@ class SwornDisclosureController extends Controller
                     'child_abuse' => $request->child_abuse,
                     'signature' => $signatureName,
                     'wit_signature' => $witSignatureName,
-                    'applicant_id' => $userID
                 ]
             );
 
             DB::commit();
 
-            if (!File::exists($singaturePath)) {
-                File::makeDirectory($singaturePath, 0755, true);
-            }
-
+            // Save files
             Storage::disk('public')->put('signature/' . $signatureName, $signatureBinary);
             Storage::disk('public')->put('signature/' . $witSignatureName, $witSignatureBinary);
 
             return response()->json(['message' => 'Sworn Disclosure Statement Signed Successfully']);
         } catch (Exception $ex) {
+
             DB::rollBack();
             Log::error($ex->getMessage());
-            return response()->json(['message' => 'An unexpected error occurred']);
+
+            return response()->json(['message' => 'An unexpected error occurred'], 500);
         }
     }
 }
